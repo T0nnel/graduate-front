674f603b66b3304b43e54fb6d15603b76c7422ee
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-07-26 23:42:48 +0300

    dhvladv;ausd;

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,5 +6,28 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  // other configurations...
-};
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-transform-runtime']
+              }
+            },
+            {
+              loader: 'ts-loader'
+            }
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }
+  };
+  
